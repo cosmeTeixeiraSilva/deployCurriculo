91f3b300e99159359e9406ca6366f3122fb8fdbe
CosmeTSilva <133916990+CosmeTSilva@users.noreply.github.com>
2025-01-27 17:24:19 -0300

    construção de hero

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 //recarregando o cache em 10 segundos 
 export const revalidate = 10;
@@ -6,10 +7,19 @@ export default function Home() {
     <div className="flex flex-col justify-center items-start">
       {/*buton1*/}
 
-      <a href={"/cliente"} className="bg-blue-800 rounded p-3 m-3">clientes </a>
-      <h3 className="text-white">Atualizando meu Sistema </h3>
-      <div className="w-full h-80 bg-white">
+      <a href={"/cliente"} className="bg-blue-800 rounded p-3 m-3 text-white">clientes </a>
 
+      <div className="relative w-1/2 max-w-1/2 h-80 bg-white mx-auto rounded border border-white overflow-hidden">
+        <Image
+          src="/uber1.jpg"
+          alt="image1"
+          layout="fill" // Faz a imagem ocupar todo o container
+          objectFit="cover" // Ajusta a imagem proporcionalmente para cobrir o container
+          className="absolute top-0 left-0 z-0"
+        />
+        <span className="absolute z-10 bg-white/60 text-black p-4 rounded bottom-5 right-5 font-bold text-xl">
+          Com Uber você vai mais longe...
+        </span>
       </div>
 
     </div>
