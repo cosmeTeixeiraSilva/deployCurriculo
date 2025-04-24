61d5100e1e05b3fc339ed996fb77d796010262ea
Eng Cosme  - Docente Senac/MG <71655160+cosmeTeixeiraSilva@users.noreply.github.com>
2025-04-23 23:30:40 -0300

    Update header.jsx

--- app/login/_components/header.jsx
+++ app/login/_components/header.jsx
@@ -12,7 +12,7 @@ export default function HeaderLogin() {
           height={50}
           className="mx-auto  p-2    " // Adicionando drop-shadow-2xl
         />
-        <p className=" text-3xl font-bold mt-2  text-white ">SENAC X</p>
+        <p className=" text-2xl font-bold mt-2  !text-white ">SENAC X</p>
         <Image
           src={"/foguete.png"}
           alt="foguete"
